f6ef88a90010f26596e153b283a64eea2a2ae4fa
rebecca maria <rebecca@bigum.dk>
2025-09-13 13:30:04 +0200

    conveyer belt capasity check

--- main.c
+++ main.c
@@ -1,23 +1,21 @@
 
 #include <stdio.h>
 int coneyer(){
-int motorer;
-float pakkevaegt;
-float motorcapasitet=5.6;
+    int motorer;
+    float pakkevaegt;
+    float motorcapasitet=5.6;
 
-        printf("Hvor mange motorer er der? input helt tal \n");
-        scanf("%d", &motorer);
-        
-        printf("Hvor mange kg vejer pakkerne tilsammen? input tal \n");
-        scanf("%f", &pakkevaegt);
-        float maxkapacitet = motorcapasitet * motorer;
-        if(pakkevaegt<=maxkapacitet){
-            printf("Conveyer kan godt baere det \n");
-        }else{
-            printf("Conveyer kan ikke baere det \n");
-        }
-
+    printf("Hvor mange motorer er der? input helt tal \n");
+    scanf("%d", &motorer);
     
+    printf("Hvor mange kg vejer pakkerne tilsammen? input tal \n");
+    scanf("%f", &pakkevaegt);
+    float maxkapacitet = motorcapasitet * motorer;
+    if(pakkevaegt<=maxkapacitet){
+        printf("Conveyer kan godt baere det \n");
+    }else{
+        printf("Conveyer kan ikke baere det \n");
+    }    
 }
 
 int main() {
